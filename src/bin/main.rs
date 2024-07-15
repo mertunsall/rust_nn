93b473492ff05d1331ff4351478ef7cde3826a1c
mertunsall <mertunsal1905@gmail.com>
2024-07-15 20:42:41 +0300

    use dataset generation function

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,19 +6,12 @@ use rust_nn::functions::{normal, ones_like, print_matrix, print_vector, matmul, 
 use rust_nn::linear::Linear;
 //use rust_nn::relu::Relu;
 use rust_nn::mseloss::MSELoss;
-
-// initialize a tensor of batch size 2, 2 input features
-// initialize a linear layer with 2 input features and 1 output feature
-// forward pass the input tensor through the linear layer
-// print the input and the output
-
-// TODO: make everything seeded
-
+use rust_nn::utils::{create_linear_dataset};
 
 fn main() {
 
     // set up input, batch size, input dim, output dim
-    let batch_size = 1000;
+    let batch_size = 300;
     let input_dim = 50;
     let output_dim = 1; 
 
@@ -26,30 +19,24 @@ fn main() {
     const SEED: u64 = 41;
     let mut rng = StdRng::seed_from_u64(SEED); 
 
-    // create the data
-    let input = normal(batch_size, input_dim, &mut rng, 0.0, 1.0);
-    let std = 1.0 / (input_dim as f64).sqrt();
-    let weight = normal(input_dim, output_dim, &mut rng, 0.0, std);
-    let bias = normal(1, output_dim, &mut rng, 0.0, std)[0].clone();
-    // y = Wx + b
-    let y_true = matmul(&input, &weight);
-    let y_true = add_vec_to_matrix(&y_true, &bias);
+    let noise_std = 0.3;
+    let (weight, bias, x, y) = create_linear_dataset(batch_size, input_dim, output_dim, noise_std, &mut rng);
 
     // create the layers and initialize loss
     let mut linear_layer1 = Linear::new(input_dim, output_dim, &mut rng);
     let loss_fn = MSELoss::new();
 
     // do forward and backward pass
-    let lr = 0.002;
-    for i in 0..1001 {
-        let output1 = linear_layer1.forward(&input);
-        let loss = loss_fn.forward(&output1, &y_true);
+    let lr = 0.005;
+    for i in 0..2001 {
+        let output1 = linear_layer1.forward(&x);
+        let loss = loss_fn.forward(&output1, &y);
     
-        let gradient = loss_fn.backward(&output1, &y_true);
-        let grads1 = linear_layer1.backward(&gradient, &input);
+        let gradient = loss_fn.backward(&output1, &y);
+        let grads = linear_layer1.backward(&gradient, &x);
         //let gradient = grads1.0;
-        let gradient_weight = grads1.1;
-        let gradient_bias = grads1.2;
+        let gradient_weight = grads.1;
+        let gradient_bias = grads.2;
 
         // update weights
         for i in 0..input_dim {
@@ -65,7 +52,7 @@ fn main() {
             linear_layer1.bias[idx] -= lr * gradient_bias[idx];
         }
 
-        if i%100 == 0 {
+        if i%200 == 0 {
             println!("Epoch: {}", i);
             println!("Loss: {:.2}", loss);
             println!("");
@@ -75,13 +62,13 @@ fn main() {
 
     /*
     println!("Input: ");
-    print_matrix(&input, 2);
+    print_matrix(&x, 2);
     println!("True Weight: ");
     print_matrix(&weight, 2);
     println!("True Bias: ");
     print_vector(&bias, 2);
     println!("True Output:");
-    print_matrix(&y_true, 2);
+    print_matrix(&y, 2);
 
 
     println!("Linear Layer:");
